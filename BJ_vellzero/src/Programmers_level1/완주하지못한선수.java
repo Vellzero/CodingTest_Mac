78bde895db5c79b79a3feeb793125e716c5dc450
kjy93913 <kjy_@naver.com>
2021-04-27 21:01:08 +0900

    프로그래머스 lv1 크레인,완

--- BJ_vellzero/src/Programmers_level1/완주하지못한선수.java
+++ BJ_vellzero/src/Programmers_level1/완주하지못한선수.java
@@ -1,5 +1,6 @@
+package Programmers_level1;
 import java.util.Arrays;
-class Solution {
+class 완주하지못한선수 {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         String temp = "";
--- BJ_vellzero/src/Programmers_level1/크레인인형뽑기.java
+++ BJ_vellzero/src/Programmers_level1/크레인인형뽑기.java
@@ -1,5 +1,7 @@
 package Programmers_level1;
-class 크레인인형뽑기 {
+import java.util.Stack;
+
+class Solution {
 	public static void main(String args[]) {
 		
 	int board [][] = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
@@ -10,6 +12,37 @@ class 크레인인형뽑기 {
 	}
     public static int solution(int[][] board, int[] moves) {
         int answer = 0;
+        
+        Stack <Integer> stack = new Stack<>(); 
+        
+        
+        
+        for(int i=0; i<moves.length;i++) {
+        		int a = moves[i];
+        		int b = 0;
+        		while(true) {
+        			if(b>=board[0].length) {
+        				break;
+        			}
+        			if(board[b][a-1]!=0) {
+        				if(stack.size()!=0 && stack.peek()== board[b][a-1]) {
+        					stack.pop();
+        					answer+= 2;
+        				}
+        				else {
+        					stack.add(board[b][a-1]);
+        				}
+        				board[b][a-1] =0;
+        				break;
+        			}
+        			b++;
+        		}
+        				
+        }
+        
+        
+        
+        
         return answer;
     }
 }
