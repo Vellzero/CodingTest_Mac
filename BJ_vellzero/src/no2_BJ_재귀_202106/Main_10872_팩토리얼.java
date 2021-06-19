a0b50340c6aee55f7a4ea8398c2ba988f9bd59b0
kjy93913 <kjy93913@github.com>
2021-06-19 10:05:11 +0900

    백준_재귀_팩토리얼 2021년 6월 19일 (토) 10:05

---
+++ BJ_vellzero/src/no2_BJ_재귀_202106/Main_10872_팩토리얼.java
@@ -0,0 +1,23 @@
+package no2_BJ_재귀_202106;
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		 
+		int A = sc.nextInt();
+		
+		factorial(A);
+		sc.close();
+	}
+	
+	public static void factorial(int n) {
+		int sum = 1;
+		for(int i=1;i<n;i++) {
+			sum = sum * i;
+		}
+ 
+		System.out.print(sum);
+	}
+ 
+}
