a244f9512a8bd2a314df48fe236ab139e4ca71be
kjy93913 <kjy93913@github.com>
2021-06-19 10:13:45 +0900

    백준 재귀 2021년 6월 19일 토 10시 13분

--- BJ_vellzero/src/no2_BJ_재귀_202106/Main_10872_팩토리얼.java
+++ BJ_vellzero/src/no2_BJ_재귀_202106/Main_10872_팩토리얼.java
@@ -1,7 +1,7 @@
 package no2_BJ_재귀_202106;
 import java.util.Scanner;
  
-public class Main {
+public class Main_10872_팩토리얼 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		 
