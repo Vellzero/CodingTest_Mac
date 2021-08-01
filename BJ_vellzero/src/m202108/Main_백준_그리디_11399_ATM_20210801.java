67cea594466ea6f3de800feb5fe0710a5c1c695c
kjy93913 <kjy_@naver.com>
2021-08-01 10:39:02 +0900

    백준_그리디_ATM _ 2021년 8월 1일 일요일 10:39

--- BJ_vellzero/src/m202108/Main_백준_그리디_11399_ATM_20210801.java
+++ BJ_vellzero/src/m202108/Main_백준_그리디_11399_ATM_20210801.java
@@ -1,7 +1,8 @@
+package m202108;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public class Main_백준_그리디_11399_ATM_20210801 {
  
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
