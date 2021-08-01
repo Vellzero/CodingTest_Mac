0129354fa21da32aec47468c07ec0468c4f70920
kjy93913 <kjy_@naver.com>
2021-08-01 16:50:37 +0900

    백준_백트래킹_N과M4문제 비교_2021년 8월 1일 일요일 16:50

---
+++ BJ_vellzero/src/m202108/Main_백준_백트래킹_15651_N과M3_20210801.java
@@ -0,0 +1,42 @@
+package m202108;
+import java.util.Arrays;
+import java.util.Scanner;
+ 
+public class Main_백준_백트래킹_15651_N과M3_20210801 {
+ 
+	public static int[] arr;
+	public static int N,M;
+	
+	public static void main(String[] args) {
+ 
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		arr = new int[M];
+		
+		dfs(0);
+		sc.close();
+	}
+	
+	public static void dfs(int depth) {
+		
+		if(depth == M) {
+			for(int val : arr) {
+				System.out.print(val + " ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for(int i =1; i<=N; i++) {
+			arr[depth] = i;
+			dfs(depth +1);
+			
+ 		}
+			
+	}
+		
+	
+}
---
+++ BJ_vellzero/src/m202108/Main_백준_백트래킹_15652_N과M4_20210801.java
@@ -0,0 +1,42 @@
+package m202108;
+
+import java.util.Scanner;
+ 
+public class Main_백준_백트래킹_15652_N과M4_20210801 {
+ 
+	public static int[] arr;
+	public static int N,M;
+	
+	public static void main(String[] args) {
+ 
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		arr = new int[M];
+		
+		dfs(1, 0);
+		sc.close();
+	}
+	
+	public static void dfs(int A, int depth) {
+		
+		if(depth == M) {
+			for(int val : arr) {
+				System.out.print(val + " ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for(int i =A; i<=N; i++) {
+			arr[depth] = i;
+			dfs(i, depth +1);
+			
+ 		}
+			
+	}
+		
+	
+}
