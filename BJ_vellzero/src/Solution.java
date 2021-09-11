bbabea9fa47de1fdc75a465605cbd3c5375a85d7
kjy93913 <kjy_@naver.com>
2021-09-11 19:44:54 +0900

    카카오풀다가

--- BJ_vellzero/src/Solution.java
+++ BJ_vellzero/src/Solution.java
@@ -1,35 +1,13 @@
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.ArrayList;
 
 class Solution {
-	
-	public static void main(String[] args) {
-		
-		String s = "{{2},{2,1},{2,1,3},{2,1,3,4}}";
-		
-		int [] test = solution(s);
-		System.out.println(Arrays.toString(test));
-		
-	}
-	
-	public static int[] solution(String s) {
-		int[] answer = {};
-		
-        String [] strs = s.replaceAll("[{}]", " ").trim().split(" ,");
-       
-        answer = new int[strs.length];
-        
-        HashSet<Integer> hs = new HashSet<Integer>();
-        Arrays.sort(strs, (a,b)->(a.length()-b.length()));
-        int i = 0;
-        for(String str : strs){
-            for(String st : str.split(",")){
-                int a  = Integer.parseInt(st.trim());
-                if(hs.contains(a))continue;
-                hs.add(a);
-                answer[i++] = a;
-            }
-        }
+    public int[] solution(String[] id_list, String[] report, int k) {
+    	
+    	
+    	ArrayList <Integer> A = new ArrayList<>();
+    	//
+    	
+        int[] answer = {};
         return answer;
     }
 }
