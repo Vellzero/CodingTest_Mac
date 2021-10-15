f1d58e328a477acc2b7c67dba8660b9ddf75f92e
kjy93913 <kjy_@naver.com>
2021-10-16 00:00:02 +0900

    잔디용

--- BJ_vellzero/src/Main.java
+++ BJ_vellzero/src/Main.java
@@ -6,7 +6,7 @@ public class Main{
 	static boolean visit[];
 	static int n,m,v;
 	static int count =0;
-	//
+	////
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
