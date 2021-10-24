961d5650f1b4547413ceeba2d4273991a937e677
kjy93913 <kjy_@naver.com>
2021-10-24 10:08:06 +0900

    백준 백트래킹 N과M 다시풀기 _ 2021년 10월 24일 10

--- BJ_vellzero/src/m202110/Main_백준_15649_N과M1_백트래킹_실버3_다시풀기_20211024일.java
+++ BJ_vellzero/src/m202110/Main_백준_15649_N과M1_백트래킹_실버3_다시풀기_20211024일.java
@@ -1,46 +1,44 @@
+package m202110;
 import java.util.Scanner;
  
-public class Main {
+public class Main_백준_15649_N과M1_백트래킹_실버3_다시풀기_20211024일 {
 	
 	public static int N;
 	public static int M;
-	
+ 
 	public static int[] arr;
 	public static boolean[] visit;
-	
+ 
 	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
-		
-		N = sc.nextInt();
-		M = sc.nextInt();
-		
+ 
+		Scanner in = new Scanner(System.in);
+ 
+		N = in.nextInt();
+		M = in.nextInt();
+ 
 		arr = new int[M];
 		visit = new boolean[N];
-		
 		dfs(0);
-		sc.close();
+ 
 	}
+ 
 	public static void dfs(int depth) {
-		if(depth == M) {
-			for(int i=0; i<arr.length;i++) {
+		if (depth == M) {
+			for (int i=0; i<arr.length;i++) {
 				System.out.print(arr[i] + " ");
 			}
 			System.out.println();
 			return;
 		}
-		
-		for(int i=0; i<N;i++) {
-			if(!visit[i]) {
+ 
+		for (int i = 0; i < N; i++) {
+			if (!visit[i]) {
 				visit[i] = true;
-				arr[depth] = i+1;
-				dfs(depth+1);
-				visit[i] =false;
+				arr[depth] = i + 1;
+				dfs(depth + 1);
+				visit[i] = false;
 			}
 		}
-		
 	}
+ 
 }
-
-
-
-
