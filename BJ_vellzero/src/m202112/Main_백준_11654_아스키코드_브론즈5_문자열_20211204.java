78467a50b48e982f61be7c89e60118a38d42e722
kjy93913 <kjy_@naver.com>
2021-12-04 12:51:16 +0900

    백준 문자열부터 다시 풀어보기 _ 2021년 12월 4일 토요일  12시 50

--- BJ_vellzero/src/m202112/Main_백준_11654_아스키코드_브론즈5_문자열_20211204.java
+++ BJ_vellzero/src/m202112/Main_백준_11654_아스키코드_브론즈5_문자열_20211204.java
@@ -1,6 +1,7 @@
+package m202112;
 import java.util.Scanner;
 
-public class Main{
+public class Main_백준_11654_아스키코드_브론즈5_문자열_20211204{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
