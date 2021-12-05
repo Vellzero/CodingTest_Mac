7f184adbeebe1faef4fd9c3b65ef919cc3adf0b4
kjy93913 <kjy_@naver.com>
2021-12-05 22:39:36 +0900

    잔디

--- BJ_vellzero/src/Main.java
+++ BJ_vellzero/src/Main.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main {
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		//
+		char A = sc.next().charAt(0);
 		
-		char a = sc.nextLine().charAt(0);
+		int B = (int)A;
 		
-		int b = (int)a;
-		
-		System.out.println(b);
+		System.out.println(A);
+		System.out.println(B);
+
 	}
 }
