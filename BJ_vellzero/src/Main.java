e2835943d71b8930f6cc6c4c7d34cca07c69b542
kjy93913 <kjy_@naver.com>
2021-12-30 23:07:19 +0900

    잔

--- BJ_vellzero/src/Main.java
+++ BJ_vellzero/src/Main.java
@@ -14,7 +14,7 @@ public class Main {
   n = sc.nextInt();//
   m = sc.nextInt();////
   start = sc.nextInt();////
-  ////
+  //////
   check = new int[1001][1001]; //좌표를 그대로 받아들이기 위해 +1해서 선언
   checked = new boolean[1001]; //초기값 False
   
